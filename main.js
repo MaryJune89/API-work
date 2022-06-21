dc5311c5176a5456252d752e29f96aff6d854d2d
Mary June <98911428+MaryJune89@users.noreply.github.com>
2022-06-21 12:36:03 -0400

    Building with NASAs API

---
+++ main.js
@@ -0,0 +1,28 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch() {
+    let pick = document.querySelector('input').value
+    console.log(pick)
+
+    const url = `https://api.nasa.gov/planetary/apod?api_key=SIpFeDBIKchkPUerawIZUCa9LIYKwL62hwyeGfXS&date=${pick}`
+
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data)
+            if (data.media_type === 'image') {
+                document.querySelector('img').src = data.hdurl
+            } else if (data.media_type === 'video') {
+                document.querySelector('iframe').src = data.url
+            }
+
+
+            document.querySelector('h3').innerText = data.explanation
+
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+
+}
